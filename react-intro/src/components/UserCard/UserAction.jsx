5f072e05f9f35521e37b5abdf746f8f591d5f600
Mohan Singh <mslogicmaster@gmail.com>
2025-08-06 07:56:44 +0530

    click event

--- react-intro/src/components/UserCard/UserAction.jsx
+++ react-intro/src/components/UserCard/UserAction.jsx
@@ -4,16 +4,28 @@ import Button from '../Button';
 export default function UserAction(props) {
 	const user = props.user;
 	const userId = user.id;
-	const onDelete = props.onDelete;
+	// const onDelete = props.onDelete;
+
+ const handleDelete = (userId, event, name) => {
+	console.log('delete', userId, event, name);
+ }
+
 
   return (
 		<div className="flex flex-row gap-2 justify-end items-center">	
 		<NavLink to={`/users/${userId}`}>View</NavLink>
 		<NavLink to={`/users/${userId}/update`}>Edit</NavLink>
 		<div>
-			<Button onClick={() => {
-				onDelete(userId);
-			}}>Delete</Button>
+			{/* <Button onClick={function(event){
+			handleDelete(userId, event)
+			}}>Delete</Button>  */}
+			{/* pass additional argument to the function using arrow function/arrow notation */}
+
+			{/* <Button onClick={(event)=> {
+			handleDelete(userId, event)
+			}}>Delete</Button> */}
+
+			<Button onClick={(event)=>handleDelete(userId, event, user.name)}>Delete</Button>
 		</div>
 		</div>
   )
